b803ed7c5509c7e73c2085c52e8fb99d76c62591
iskhakmuhamad <iskhakmuhama@gmail.com>
2025-09-12 15:58:27 +0700

    feat: new task notification

--- internal/usecase/notification_usecase.go
+++ internal/usecase/notification_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"context"
+	"log"
+
 	"github.com/iskhakmuhamad/mylogs-ws/internal/delivery/ws"
 	"github.com/iskhakmuhamad/mylogs-ws/internal/entity"
 	"github.com/iskhakmuhamad/mylogs-ws/internal/repository"
@@ -10,6 +13,7 @@ type NotificationUsecase interface {
 	Create(n *entity.Notification) error
 	List(userID uint, unreadOnly bool) ([]entity.Notification, error)
 	MarkAsRead(id uint) error
+	HandleIncomingNotification(ctx context.Context, notif entity.Notification) error
 }
 
 type notificationUsecase struct {
@@ -29,6 +33,20 @@ func (u *notificationUsecase) Create(n *entity.Notification) error {
 	u.hub.Broadcast(n.UserID, map[string]any{
 		"type": "notification",
 		"data": n,
+	})
+
+	return nil
+}
+
+func (u *notificationUsecase) HandleIncomingNotification(ctx context.Context, notif entity.Notification) error {
+	log.Println("check ,", notif)
+	if err := u.repo.Create(&notif); err != nil {
+		return err
+	}
+
+	u.hub.Broadcast(notif.UserID, map[string]any{
+		"type": "notification",
+		"data": notif,
 	})
 
 	return nil
